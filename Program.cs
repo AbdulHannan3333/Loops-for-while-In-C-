4fa6e612c00d3ef98bd65e9c4e5f4e5a4278bf1e
AbdulHannan3333 <hannan67212@outlook.com>
2024-06-16 01:25:29 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops__for__while_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //For Loop
+            int num = 10;
+            for (int i = 0; i <num; i++) 
+            {
+                Console.WriteLine(i);
+            }
+            //While Loop
+            int num1 = 1;
+            while (num1 < 10) 
+            {
+                Console.WriteLine(num1);
+                num1++;
+            }
+
+        }
+    }
+}
